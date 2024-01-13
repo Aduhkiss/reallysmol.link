ec19f84885ac3604e185b23a5c095e340d313971
Aduhkiss <zambranaatticus@gmail.com>
2024-01-13 03:18:14 -0800

    Worked and added some stuff

---
+++ README.md
@@ -0,0 +1,11 @@
+# Really Smol Link
+
+This repository serves as the source code for reallysmol.Link
+
+a super easy to use link shortening service designed by Atticus
+
+
+Feel free to make your own contributions, and create a pull request. If I feel they are necessary, I will merge
+them to the master branch.
+
+Please credit yourself in the files you make/edit
--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 require('common.php');
+
+/*
+
+		reallysmol.link
+		
+		Made with <3 by Atticus Zambrana
+
+*/
+
 ?>
 
 <html>
@@ -24,37 +33,77 @@ require('common.php');
       }
       h1 {
         font-family: 'Open Sans', sans-serif;
-        font-size: 50px;
+        font-size: 70px;
         text-align: center;
       }
       p, form {
         text-align: center;
+		font-size: 20px;
+		font-family: 'Open Sans', sans-serif;
       }
       .big {
         border: 3px solid purple;
         border-radius: 25px;
         padding: 20px; 
         width: 50%;
-        height: 55%;
+        height: 60%;
         margin: auto;
       }
       .bottom-text {
         font-size: 20px;
       }
+	  .button {
+	  text-align: center;
+	  font: bold 11px Arial;
+	  text-decoration: none;
+	  background-color: #EEEEEE;
+	  color: #333333;
+	  padding: 2px 6px 2px 6px;
+	  border-top: 1px solid #CCCCCC;
+	  border-right: 1px solid #333333;
+	  border-bottom: 1px solid #333333;
+	  border-left: 1px solid #CCCCCC;
+	}
     </style>
   </head>
 
   <body>
     <div class="big">
-    <h1>Welcome to ReallySmol.Link!</h1>
+    <h1>ReallySmol.Link!</h1>
     <hr>
-    <p>A drop dead simple link shortener for pros</p>
+    <p>A super easy to use link shortener that anyone can use</p>
     <br>
-
-    <form action="/action/shorten.php" method="POST">
+	
+	<?php
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$banned = false;
+	$ban_reason = "";
+	$check = $db->execute_query("SELECT * FROM `banned_ips` WHERE `ip` = ?", [$ip]);
+	foreach($check as $row) {
+		$banned = true;
+		$ban_reason = $row['reason'];
+	}
+	
+	
+	/*
+		adding a "ban system"
+		i'm aware this is incredibly easy to evade if you know how to make basic HTTP POST requests manually, and will
+		patch that out later. but i dont feel like it right now.
+		
+		...unless you want to o_O
+	*/
+	
+	if($banned) {
+		echo '<div class="message-box"><p><strong>You are banned from this service! </strong> ' . $ban_reason . '</p></div><br><br>';
+	} else {
+		echo '<form action="/action/shorten.php" method="POST">
       <input type="text" name="url" placeholder="Link to Shorten!">
       <input type="submit" value="Shorten!">
-    </form><br>
+    </form><br>';
+	}
+	?>
+
+    
 
     <?php
     if(isset($_GET['go'])) {
@@ -82,6 +131,11 @@ require('common.php');
 
     <br><br><br><br><br><br><br>
     <i><p class="bottom-text">Made with ❤️ by Atticus Zambrana</p></i>
+	<!-- I can't fucking figure out how to center these, someone else please deal with it -->
+	<a href="https://twitter.com/Aduhkiss" class="button">Twitter / X</a>
+	<a href="https://bsky.app/profile/thecloudyco.com" class="button">Blue Sky</a>
+	<a href="https://github.com/Aduhkiss" class="button">GitHub</a>
+	<a href="https://www.youtube.com/channel/UCbFbmh83WSgOtz7sV9bVSwg" class="button">YouTube</a>
   </div>
   </body>
 
